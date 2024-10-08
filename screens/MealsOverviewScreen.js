097651d20a0b14dc220e8287ee1ae21115e0b17a
amr4356 <amrooo73321@gmail.com>
2024-10-08 10:14:10 +0300

    some adjustments

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,11 +1,11 @@
 import { StyleSheet, Text, View } from "react-native";
-import { useRoute } from "@react-navigation/native";
+// import { useRoute } from "@react-navigation/native";
 
 import { MEALS } from "../data/dummy-data";
 
 function MealsOverviewScreen({ route }) {
-    const route=useRoute();
-    route.params.categoryId
+    // const route=useRoute();
+    // route.params.categoryId
     const categoryId=route.params.categoryId;
   return (
     <View style={styles.container}>
